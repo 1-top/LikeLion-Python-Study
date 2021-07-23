97577e61982db3a8898d8d72812b841402c2b171
Siyoung <happycloudy0308@gmail.com>
2021-07-24 01:44:33 +0900

    Study 클래스,모듈,패키지,예외처리,내장함수.py-시영

--- 시영/클래스/README.md
+++ 시영/클래스/README.md
@@ -107,5 +107,169 @@ class MoreFourCal(FourCal):
 - '클래스 이름. 클래스 변수' 로 사용
 - 클래스 변수는 클래스로 만든 모든 객체에 공유됨
 
+------
+
+## 모듈
+
+###모듈 만들기  
+- .py로 만든 파일은 모두 모듈
+
+###모듈 불러오기  
+- .py 파일을 불러오기 위해 import 사용
+- 예시) import mod1 (뒤에 .py를 붙이지 않음)
+- import의 사용방법은 다음과 같음
+  ```python
+  import 모듈이름
+  ```
+- 모듈 이름 없이 함수 이름만 쓰고 싶은 경우, 다음과 같이 작성
+```python
+from 모듈 이름 import 모듈 함수
+```
+
+### if __name__ == "_main_"
+
+- if __name__ == "_main_" 을 대화형 인터프리터나 다른 파일에서 모듈을 불러서 사용할 때는
+거짓이 되어 if 다음 문장이 수행되지 않음  
+    
+
+- __name__ 변수란?  
+직접 파일을 실행하는 경우, _name_변수에는 _main_값이 저장됨
+  하지만 파이썬 셸이나 모듈에서 import 하는 경우에, __name__ 변수에 모듈 이름 값 mod1이 저장됨
+  
+### 클래스나 변수 등을 포함한 모듈
+
+- 모듈은 클래스나 변수를 포함할 수 있음
+```python
+PI = 3.141592
+
+class Math: 
+    def solv(self, r): 
+        return PI * (r ** 2) 
+
+def add(a, b): 
+    return a+b 
+
+>>> a = mod2.Math()
+>>> print(a.solv(2))
+12.566368
+
+>>> print(mod2.add(mod2.PI, 4.4))
+7.541592
+ ```
+
+### 다른 파일에서 모듈 불러오기
+```python
+import mod2
+result = mod2.add(3, 4)
+print(result)
+ ```
+- import로 모듈 불러와서 사용 가능(동일한 디렉터리 안에 있어야 함)
+
+
+- 모듈을 불러오는 다른 방법들
+
+1. sys.path.append  
+우선 import sys를 불러오고 sys.path라고 입력하면 디렉터리를 보여줌  
+   이후 sys.path.append를 사용해서 추가함  
+     
+
+2. PYTHONPATH 환경 변수 사용하기
+디렉터리 이동이나 모듈 추가 없이 set PYTHONPATH를 사용하면 모듈이 불러짐
+   
+-----------------------
+
+## 패키지
+예를 들어 A.B의 경우 A는 패키지 이름이고 B는 A 패키지의 B모듈이 됨
+
+## 패키지 만들기, 실행하기
+
+1. import로 실행
+2. from ...import로 실행
+3. 모듈을 직접 import해서 실행
+
+
+## __init__.py의 용도
+
+- __init__.py는 디렉토리가 패키지의 일부임을 알려주는 역할
+- * 사용해서 import 할 때 __init__.py 파일에 __all__변수 설정하고 
+    import 할 수 있는 모듈을 정의해 주어야 함
+    
+## relative 패키지
+
+- from game.sound.echo import echo_test 를 from ..sound.echo import echo_test 로 변경
+- ..은 부모 디렉터리를 의미함 / .은 현재 디렉터리를 의미함
+
+--------------------------
+
+## 예외 처리
+- 오류를 무시하고 싶을 때 try와 except를 사용해서 예외적으로 오류를 처리할 수 있게 해줌
+
+## 오류가 나타나는 상황
+- 디렉토리에 없는 파일을 열려고 할 때
+- 0으로 다른 숫자를 나누는 경우
+- 리스트에 없는 값을 찾을 때
+
+## 오류 예외 처리 기법
+- try, except 문
+
+1. try, except만 쓰는 방법
+
+```python
+try:
+    ...
+except:
+    ...
+ ```
+- 오류가 발생하면 except 수행
+
+2. 발생 오류만 포함한 except문
+```python
+try:
+    ...
+except 발생 오류:
+    ...
+ ```
+- except 문에 미리 정해놓은 오류 이름하고 일치할 때만 except 블록 수행
+
+3. 발생 오류와 오류 메시지 변수까지 포함한 except문
+```python
+try:
+    ...
+except 발생 오류 as 오류 메시지 변수:
+    ...
+ ```
+- 두 번째 경우에서 오류 메시지의 내용까지 알고 싶을 때 사용
+
+## try finally
+- finally 절은 try문 예외 발생 여부에 상관 없이 항상 수행됨
+- finally 절은 사용한 문장을 close 할 때 많이 사용함
+```python
+f = open('foo.txt', 'w')
+try:
+    # 무언가를 수행한다.
+finally:
+    f.close()
+ ```
+- try문의 예외 발생 여부와 상관 없이 finally 절 수행
+
+## 여러개의 오류 처리하기
+```python
+try:
+    ...
+except 발생 오류1:
+   ... 
+except 발생 오류2:
+   ...
+ ```
+## 오류 회피하기
+-pass 사용
+
+## 오류 일부러 발생시키기
+- 자식 클래스가 특정 함수를 구현하지 않고 호출하면 오류 발생
+
+---------------------
+## 내장 함수
+- 모듈과 달리 import가 필요하지 않기 때문에 설정 없이 사용 가능
+- .py 파일에 정리
 
 
---
+++ 시영/클래스/클래스,모듈,패키지,예외처리,내장함수.py
@@ -0,0 +1,352 @@
+# 더하기 코드
+result = 0
+
+def add(num):
+    global result
+    result += num
+    return result
+
+print(add(3))
+print(add(4))
+
+# 클래스를 사용한 더하기
+
+class Calculator:
+    def __init__(self):
+        self.result = 0
+
+    def add(self, num):
+        self.result += num
+        return self.result
+
+cal1 = Calculator()
+cal2 = Calculator()
+
+print(cal1.add(3))
+print(cal1.add(4))
+print(cal2.add(3))
+print(cal2.add(7))
+
+# 사칙연산 클래스 만들기
+#class FourCal:
+      # def setdata(self,first,second):
+      # self.first = first
+     # self.second = second
+# a = FourCal()
+# a.setdata(4,2)
+
+
+# 더하기 기능 추가
+
+class FourCal:
+    def setdata(self, first, second):
+        self.first = first
+        self.second = second
+    def add(self):
+        result = self.first + self.second
+        return result
+
+a = FourCal()
+a.setdata(4,2)
+print(a.add())
+
+# 곱하기,뺴기, 나누기 기능 만들기
+class FourCal:
+    def setdata(self, first, second):
+        self.first = first
+        self.second = second
+    def add(self):
+        result = self.first + self.second
+        return result
+    def mul(self):
+        result = self.first * self.second
+        return result
+    def sub(self):
+        result = self.first - self.second
+        return result
+    def div(self):
+        result = self.first / self.second
+        return result
+a = FourCal()
+b = FourCal()
+a.setdata(4,2)
+b.setdata(3,8)
+print(a.add())
+print(a.mul())
+print(a.sub())
+print(a.div())
+print(b.add())
+print(b.mul())
+print(b.sub())
+print(b.div())
+
+# 생성자
+class FourCal:
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+    def setdata(self, first, second):
+        self.first = first
+        self.second = second
+    def add(self):
+        result = self.first + self.second
+        return result
+    def mul(self):
+        result = self.first * self.second
+        return result
+    def sub(self):
+        result = self.first - self.second
+        return result
+    def div(self):
+        result = self.first / self.second
+        return result
+
+a = FourCal(4,2)
+print(a.first)
+print(a.second)
+print(a.add())
+print(a.div())
+
+# 클래스의 상속
+class MoreFourCal(FourCal):
+    def pow(self):
+        result = self.first ** self.second
+        return result
+a = MoreFourCal(4,2)
+print(a.pow())
+
+# 메서드 오버라이딩
+class SafeFourCal(FourCal):
+    def div(self):
+        if self.second == 0:
+            return 0
+        else:
+            return self.first / self.second
+a = SafeFourCal(4, 0)
+print(a.div())
+
+# 클래스 변수
+class Family:
+    lastname = "김"
+print(Family.lastname)
+
+a = Family()
+b = Family()
+print(a.lastname)
+print(b.lastname)
+Family.lastname = "박"
+print(a.lastname)
+print(b.lastname)
+
+#--------------------------------
+#모듈 mod1.py
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+print(add(1,4))
+print(sub(4,2))
+
+# if _name_ == "_main_" 사용
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+if __name__ == "_main_":
+    print(add(1,4))
+    print(sub(4,2))
+
+# 클래스나 변수 등을 포함한 모듈
+
+PI = 3.141592
+
+class Math:
+    def solv(self,r):
+        return PI * (r ** 2)
+
+    def add(a , b):
+        return a+b
+a = Math()
+print(a.solv(2))
+
+#----------------------
+
+# 예외 처리
+
+try:
+    4/0
+except ZeroDivisionError as e:
+    print(e)
+
+# 여러개의 오류 처리하기
+try:
+    a = [1,2]
+    print(a[3])
+    4/0
+except ZeroDivisionError:
+    print("0으로 나눌 수 없습니다.")
+except IndexError:
+    print("인덱싱 할 수 없습니다.")
+
+# 두개 이상의 오류를 동일하게 처리하기 위해서 괄호를 묶은 예시
+
+try:
+    a = [1,2]
+    print(a[3])
+    4/0
+except (ZeroDivisionError, IndexError) as e:
+    print(e)
+
+# try문에 else절 사용하기
+try:
+    age=int(input('나이를 입력하세요: '))
+except:
+    print('입력이 정확하지 않습니다.')
+else:
+    if age <= 18:
+        print('미성년자는 출입금지입니다.')
+    else:
+        print('환영합니다.')
+
+#-----------------------------
+
+# abs(절대값)
+
+print(abs(3))
+print(abs(-3))
+print(abs(-1.2))
+
+# all(모두 참이면 true, 하나라도 거짓이면 false)
+
+print(all([1,2,3,0]))
+print(all([]))
+
+# any( 하나라도 참이 있으면 true, 모두 거짓이면 flase)
+
+print(any([1,2,3,0]))
+
+# chr(유니코드 입력받아서 해당 문자 출력)
+
+print(chr(97))
+
+# dir( 객체가 자체적으로 가지고 있는 변수, 함수)
+
+print(dir([1,2,3]))
+
+# divmod( (a,b)가 있을 때 a를 b로 나눈 몫과 나머지를 튜플 형태로 출력)
+
+print(divmod(7,3))
+
+# enumerate(자료형(리스트, 튜플, 문자열)을 입력받아 enumerate 객체를 돌려줌)
+
+for i, name in enumerate(['body', 'foo' , 'bar']):
+    print(i, name)
+
+# eval(실행 가능한 문자열을 입력받아 문자열을 실행한 결과값을 돌려주는 함수)
+
+print(eval('1+2'))
+
+# filter( 함수 이름, 반복 가능한 자료형, 자료형을 함수에 입력했을 때 반환 값이 참인 것만 돌려줌)
+
+def positive(x):
+    return x>0
+
+print(list(filter(positive, [1,-3,2,0,-5,6])))
+
+# hex( 정수 값을 입력받아 16 진수로 변환하여 돌려주는 함수)
+
+print(hex(234))
+
+# id( 객체를 입력받아 고유 주소 값(레퍼런스)을 돌려주는 함수)
+
+a=3
+print(id(3))
+print(id(a))
+b=a
+print(id(b))
+
+# input( 사용자 입력을 받는 함수)
+
+a = input()
+print(a)
+
+# int(정수 형태로 바꿔서 출력)
+
+print(int(3.4))
+
+# isinstance(첫 번째 인수: 인스턴스, 두 번째 인수: 클래스 이름, 그 클래스의 인스턴스이면 true, 거짓이면 false)
+
+class Person: pass
+a = Person()
+print(isinstance(a, Person))
+
+# len( 입력값의 길이)
+
+print(len("python"))
+
+# list( 자료형을 입력받아 리스트로 만들어서 돌려주는 함수)
+
+print(list("python"))
+
+# map( map(f,iterable) f가 수행한 결과를 묶어서 돌려주는 함수)
+
+def two_times(x):
+    return x*2
+print(list(map(two_times, [1,2,3,4])))
+
+# max( 최댓값 출력)
+
+print(max([1,2,3]))
+
+# min (최솟값 출력)
+
+print(min([1,2,3]))
+
+# oct(8진수 문자열로 바꿈)
+
+print(oct(34))
+
+# ord(유니코드 출력, chr와 반대)
+
+print(ord('a'))
+
+# pow(x의 y제곱)
+
+print(pow(2,4))
+
+# range(반복 가능한 객체로 만듦)
+
+print(list(range(5)))
+print(list(range(1,10,2))) #인수가 세 개일 때 마지막은 숫자 사이의 거리
+
+# round(반올림)
+
+print(round(4.6))
+
+# sorted(정렬해서 리스트로 출력)
+
+print(sorted([3,1,2]))
+
+# str (문자열 형태로 객체를 변환해서 출력)
+
+print(str(3))
+
+# sum(입력받은 리스트나 튜플의 합)
+
+print(sum([1,2,3]))
+
+# tuple(튜플 형태로 바꿔서 돌려줌)
+
+print(tuple("abc"))
+
+#type(자료형이 무엇인지 알려줌)
+
+print(type("abc"))
+
+# zip(동일한 개수로 이루어진 자료형을 묶어주는 역할)
+
+print(list(zip([1,2,3],[4,5,6])))
